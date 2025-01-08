3d88952843b7e9fb313f382a6802863fc96fba7a
cpinom <cpinom@inacap.cl>
2025-01-08 11:24:16 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APP-INACAP-V8-BETA',
+  webDir: 'www'
+};
+
+export default config;
